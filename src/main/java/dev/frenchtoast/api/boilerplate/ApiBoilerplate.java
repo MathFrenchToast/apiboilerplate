47d920ed31ec884a03fbb0107d4b3aa97569fccc
MathFrenchToast <77589564+MathFrenchToast@users.noreply.github.com>
2024-03-22 16:16:12 +0100

    add: settings

---
+++ src/main/java/dev/frenchtoast/api/boilerplate/ApiBoilerplate.java
@@ -0,0 +1,15 @@
+/*
+ */
+
+package dev.frenchtoast.api.boilerplate;
+
+/**
+ *
+ * @author MathieuVeron(Stduser
+ */
+public class ApiBoilerplate {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
